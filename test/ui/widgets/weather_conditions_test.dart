4b9b4337958425b4f325e2e64541c8aeecc093d9
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-07-30 00:50:44 +0200

    added tests for all weather condition cases

--- test/ui/widgets/weather_conditions_test.dart
+++ test/ui/widgets/weather_conditions_test.dart
@@ -4,9 +4,139 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app_example_data_models_core/weather_app_example_data_models_core.dart';
 
 void main() {
-  const WeatherCondition _weatherCondition = WeatherCondition.hail;
   group('Weather Conditions Widget', () {
-    testWidgets('renders correctly', (WidgetTester tester) async {
+    testWidgets('renders correctly for weather condition "hail"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.hail;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+      expect(() => WeatherConditions(condition: null), throwsAssertionError);
+    });
+    testWidgets('renders correctly for weather condition "snow"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.snow;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "sleet"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.sleet;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "heavyCloud"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.heavyCloud;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "heavyRain"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.heavyRain;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "lightRain"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.lightRain;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "showers"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.showers;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "thunderstorm"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.thunderstorm;
+      await tester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: WeatherConditions(
+              condition: _weatherCondition,
+            ),
+          ),
+        ),
+      );
+      final finder = find.byWidgetPredicate((widget) =>
+          widget is Image && widget.key == Key(_weatherCondition.toString()));
+      expect(finder, findsOneWidget);
+    });
+    testWidgets('renders correctly for weather condition "unknown"',
+        (WidgetTester tester) async {
+      const WeatherCondition _weatherCondition = WeatherCondition.unknown;
       await tester.pumpWidget(
         const MaterialApp(
           home: Scaffold(
