2f34ae0633687bdd4f77c416062fec46730e3f84
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-07-29 12:38:19 +0200

    added tests for core value_objects

--- test/unit/domain/core/value_object_test.dart
+++ test/unit/domain/core/value_object_test.dart
@@ -1,43 +1,56 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc_example/domain/authentication/auth_failure.dart';
 import 'package:flutter_bloc_example/domain/authentication/value_objects.dart';
+import 'package:flutter_bloc_example/domain/core/errors.dart';
 import 'package:flutter_bloc_example/domain/core/value_failure.dart';
+import 'package:flutter_bloc_example/domain/core/value_object.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Core Value Object', () {
-    test(
-        'should check whether Authfailure.cancelledByUser().toString() is working correctly',
-        () {
-      final EmailAddress _mockValueObject = EmailAddress('Test');
-      expect(
-        _mockValueObject.value,
-        const Left(InvalidEmail(failedValue: 'Test')),
-      );
+    EmailAddress _mockValidValueObject;
+
+    setUp(() {
+      _mockValidValueObject = EmailAddress('foo.bar@test.com');
+    });
+    test('should check whether getter value is working correctly', () {
+      expect(_mockValidValueObject.value, right('foo.bar@test.com'));
+      expect(EmailAddress('Foo').value,
+          left(const ValueFailure.invalidEmail(failedValue: 'Foo')));
+    });
+    test('should check whether getOrCrash() is working correctly', () {
+      expect(_mockValidValueObject.getOrCrash(), 'foo.bar@test.com');
+      expect(() => EmailAddress('Foo').getOrCrash(),
+          throwsA(isA<UnexpectedValueError>()));
+    });
+    test('should check whether getOrElse() is working correctly', () {
+      expect(_mockValidValueObject.getOrElse('bar.foo@test.com'),
+          'foo.bar@test.com');
+      expect(EmailAddress('Foo').getOrElse('bar.foo@test.com'),
+          'bar.foo@test.com');
+    });
+    test('should check whether getter failureOrUnit is working correctly', () {
+      expect(_mockValidValueObject.failureOrUnit, right(unit));
+      expect(EmailAddress('Foo').failureOrUnit,
+          left(const ValueFailure.invalidEmail(failedValue: 'Foo')));
+    });
+    test('should check whether isValid is working correctly', () {
+      expect(_mockValidValueObject.isValid(), isTrue);
+      expect(EmailAddress('Foo').isValid(), isFalse);
+    });
+    test('should check whether getter hashCode is working correctly', () {
+      expect(_mockValidValueObject.hashCode, 991766199);
+      expect(EmailAddress('Foo').hashCode, 914322953);
     });
     test(
-        'should check whether Authfailure.serverError().toString() is working correctly',
+        'should check whether factory constructor UniqueId.fromUniqueString is throwing an AssertionError for null value',
         () {
-      expect(
-        const AuthFailure.serverError().toString(),
-        'AuthFailure.serverError()',
-      );
+      expect(() => UniqueId.fromUniqueString(null), throwsAssertionError);
     });
     test(
-        'should check whether Authfailure.emailAlreadyInUse().toString() is working correctly',
+        'should check whether factory constructor StringSingleLine is throwing an AssertionError for null value',
         () {
-      expect(
-        const AuthFailure.emailAlreadyInUse().toString(),
-        'AuthFailure.emailAlreadyInUse()',
-      );
-    });
-    test(
-        'should check whether Authfailure.invalidEmailAndPasswordCombination().toString() is working correctly',
-        () {
-      expect(
-        const AuthFailure.invalidEmailAndPasswordCombination().toString(),
-        'AuthFailure.invalidEmailAndPasswordCombination()',
-      );
+      expect(() => StringSingleLine(null), throwsAssertionError);
     });
   });
 }
