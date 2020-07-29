d1bfd184aad2a2c0a1706b90c528c48d8e26e866
Eddy <eduard.selenzow@smail.th-koeln.de>
2020-07-29 21:33:21 +0200

    added more expected assertions

--- test/ui/widgets/sign_in_form_test.dart
+++ test/ui/widgets/sign_in_form_test.dart
@@ -33,6 +33,21 @@ void main() {
         ),
       );
       expect(find.byKey(const Key('__Sign_In_Form__')), findsOneWidget);
+      expect(
+          find.byKey(const Key('__Sign_In_Form_List_View__')), findsOneWidget);
+      expect(
+          find.byKey(const Key('__Flutter_Logo_Container__')), findsOneWidget);
+      expect(
+          find.byKey(const Key('__Email_Text_Form_Field__')), findsOneWidget);
+      expect(find.byKey(const Key('__Password_Text_Form_Field__')),
+          findsOneWidget);
+      expect(find.byKey(const Key('__Sign_In_Form_Sign_In_Button__')),
+          findsOneWidget);
+      expect(find.byKey(const Key('__Sign_In_Form_Register_Button__')),
+          findsOneWidget);
+      expect(
+          find.byKey(const Key('__Sign_In_Form_Sign_In_With_Google_Button__')),
+          findsOneWidget);
     });
   });
 }
